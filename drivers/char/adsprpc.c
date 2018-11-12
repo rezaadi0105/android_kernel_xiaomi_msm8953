e33cb39b9f7afa754ecae30e110f30688bfa7985
root <rezaadipangestu5@gmail.com>
2018-11-12 07:26:57 +0700

    Revert "Merge remote-tracking branch 'als/kernel.lnx.3.18.r33-rel' into oreo-mr1"
    This reverts commit b2b2b8b45e426de5b2b63a9d5623982ea00d29c4.

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -256,7 +256,6 @@ struct fastrpc_file {
 	int cid;
 	int ssrcount;
 	struct fastrpc_apps *apps;
-	struct mutex map_mutex;
 };
 
 static struct fastrpc_apps gfa;
@@ -1761,8 +1760,6 @@ static int fastrpc_internal_munmap(struct fastrpc_file *fl,
 {
 	int err = 0;
 	struct fastrpc_mmap *map = NULL;
-
-	mutex_lock(&fl->map_mutex);
 	if (!fastrpc_mmap_remove(fl, ud->vaddrout, ud->size,
 				 &map)) {
 		VERIFY(err, !fastrpc_munmap_on_dsp(fl, map));
@@ -1773,7 +1770,6 @@ static int fastrpc_internal_munmap(struct fastrpc_file *fl,
 bail:
 	if (err && map)
 		fastrpc_mmap_add(map);
-	mutex_unlock(&fl->map_mutex);
 	return err;
 }
 
@@ -1783,13 +1779,9 @@ static int fastrpc_internal_mmap(struct fastrpc_file *fl,
 
 	struct fastrpc_mmap *map = NULL;
 	int err = 0;
-
-	mutex_lock(&fl->map_mutex);
 	if (!fastrpc_mmap_find(fl, ud->fd, (uintptr_t)ud->vaddrin, ud->size,
-			       ud->flags, &map)){
-		mutex_unlock(&fl->map_mutex);
+			       ud->flags, &map))
 		return 0;
-	}
 
 	VERIFY(err, !fastrpc_mmap_create(fl, ud->fd, (uintptr_t)ud->vaddrin,
 					 ud->size, ud->flags, &map));
@@ -1802,7 +1794,6 @@ static int fastrpc_internal_mmap(struct fastrpc_file *fl,
  bail:
 	if (err && map)
 		fastrpc_mmap_free(map);
-	mutex_unlock(&fl->map_mutex);
 	return err;
 }
 
@@ -2009,7 +2000,6 @@ static int fastrpc_device_release(struct inode *inode, struct file *file)
 		}
 		me->pending_free++;
 		mutex_unlock(&me->flfree_mutex);
-		mutex_destroy(&fl->map_mutex);
 		file->private_data = NULL;
 	}
 bail:
@@ -2169,7 +2159,6 @@ static int fastrpc_device_open(struct inode *inode, struct file *filp)
 						me->channel[cid].ssrcount;
 		}
 	}
-	mutex_init(&fl->map_mutex);
 	spin_lock(&me->hlock);
 	hlist_add_head(&fl->hn, &me->drivers);
 	spin_unlock(&me->hlock);
